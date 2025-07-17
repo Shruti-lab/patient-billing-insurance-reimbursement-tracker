6e11d3eec3e49a760aedbfe549e2ba55391f94a2
Shruti <124026034+Shruti-lab@users.noreply.github.com>
2025-07-17 17:43:47 +0530

    Created sequences.sql

---
+++ sql/sequences.sql
@@ -0,0 +1,6 @@
+-- ========================
+-- 2. SEQUENCES
+-- ========================
+
+CREATE SEQUENCE claims_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE claim_audit_seq START WITH 1 INCREMENT BY 1;
